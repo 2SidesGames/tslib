115d8eb97637299821762d3d4bd963115a92965d
misael-gdev <mslsanchez@hotmail.com>
2026-07-14 12:25:44 +0200

    Added tooltips

--- Runtime/ai/behaviour/utilityai/UtilityAIControllerData_So.cs
+++ Runtime/ai/behaviour/utilityai/UtilityAIControllerData_So.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 )]
 public class UtilityAIControllerData_So : ScriptableObject
 {
-    [SerializeField, Min(1)] private int bufferSize = 3; // podium size
-    [SerializeField, Min(1)] private int maxBuckets = 10;
-    [SerializeField, Range(0f, 1f)] private float minScoreRequired = 0.5f;
+    [SerializeField, Min(1), Tooltip("Number of actions that can be selected (Podium size).")]
+    private int bufferSize = 3;
+
+    [SerializeField, Min(1), Tooltip("Maximum number of buckets (defines priorities).")]
+    private int maxBuckets = 10;
+    [SerializeField, Range(0f, 1f), Tooltip("Minimum score required for actions to be selected (inclusive).")]
+    private float minScoreRequired = 0.5f;
 
     public int BufferSize => bufferSize;
     public int MaxBuckets => maxBuckets;
