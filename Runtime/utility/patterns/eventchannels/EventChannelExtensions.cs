0e21343e5a7f22ebba859e4f68c3b70da736a450
misael-gdev <mslsanchez@hotmail.com>
2026-07-14 12:57:11 +0200

    event channel extensions to wait events

---
+++ Runtime/utility/patterns/eventchannels/EventChannelExtensions.cs
@@ -0,0 +1,117 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using TSLib.Utility.Patterns.EventChannels;
+using TSLib.Utility.Patterns.EventChannels.Primitive;
+
+public static class EventChannelExtensions
+{
+    public static UniTask WaitAsync(this VoidChannel_So channel, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return UniTask.FromCanceled(cancellationToken);
+        }
+
+        var tcs = new UniTaskCompletionSource();
+        CancellationTokenRegistration registration = default;
+
+        void Handler()
+        {
+            // clean up
+            channel.Unsubscribe(Handler);
+            registration.Dispose();
+
+            tcs.TrySetResult();
+        }
+
+        void Callback()
+        {
+            // clean up
+            channel.Unsubscribe(Handler);
+            registration.Dispose();
+
+            tcs.TrySetCanceled(cancellationToken);
+        }
+
+        channel.Subscribe(Handler);
+
+        if (cancellationToken.CanBeCanceled)
+        {
+            registration = cancellationToken.Register(Callback);
+        }
+
+        return tcs.Task;
+    }
+
+    public static UniTask<T> WaitAsync<T>(this TS_ChannelT1_So<T> channel, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return UniTask.FromCanceled<T>(cancellationToken);
+        }
+
+        var tcs = new UniTaskCompletionSource<T>();
+        CancellationTokenRegistration registration = default;
+
+        void Handler(T value)
+        {
+            channel.Unsubscribe(Handler);
+            registration.Dispose();
+
+            tcs.TrySetResult(value);
+        }
+
+        void Callback()
+        {
+            channel.Unsubscribe(Handler);
+            registration.Dispose();
+
+            tcs.TrySetCanceled(cancellationToken);
+        }
+
+        channel.Subscribe(Handler);
+
+        if (cancellationToken.CanBeCanceled)
+        {
+            registration = cancellationToken.Register(Callback);
+        }
+
+        return tcs.Task;
+    }
+
+    public static UniTask<(T1, T2)> WaitAsync<T1, T2>(this TS_ChannelT2_So<T1, T2> channel, CancellationToken cancellationToken = default)
+    {
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return UniTask.FromCanceled<(T1, T2)>(cancellationToken);
+        }
+
+        var tcs = new UniTaskCompletionSource<(T1, T2)>();
+        CancellationTokenRegistration registration = default;
+
+        void Handler(T1 value1, T2 value2)
+        {
+            channel.Unsubscribe(Handler);
+            registration.Dispose();
+
+            tcs.TrySetResult((value1, value2));
+        }
+
+        void Callback()
+        {
+            channel.Unsubscribe(Handler);
+            registration.Dispose();
+
+            tcs.TrySetCanceled(cancellationToken);
+        }
+
+        channel.Subscribe(Handler);
+
+        if (cancellationToken.CanBeCanceled)
+        {
+            registration = cancellationToken.Register(Callback);
+        }
+
+        return tcs.Task;
+    }
+}
